41d56c810a90b942fd4e9696cbb50d85b4fabfd5
ShivBuda <shiva20073@hotmail.com>
2024-08-25 20:47:43 +0800

    implement new folder and file structure

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+export default function RootLayout({
+     children,
+ }: Readonly<{children: React.ReactNode}>) {
+     return (
+         <main lang="en">
+             {children}
+         </main>
+     );
+ }
+ 
